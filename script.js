b6e07fa27b520c74012ed6e3546e81a3ee50d635
Ben <127615469+EpicBen208@users.noreply.github.com>
2025-09-07 21:27:59 -0400

    fixed time :(

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+const born=new Date('09/10/1977/10:00:00')
 
-setInterval(() => {
-    let born=Date.now('Sep 10 1977 10:00:00')
-    document.getElementById('timestamp-years').innerHTML=('Years '+Math.round(((((born/1000)/60)/60)/24)/364))
-    document.getElementById('timestamp-days').innerHTML=('Days '+Math.round((((born/1000)/60)/60)/24))
-    document.getElementById('timestamp-hours').innerHTML=('Hours '+Math.round(((born/1000)/60)/60))
-    document.getElementById('timestamp-minutes').innerHTML=('Minutes '+Math.round((born/1000)/60))
-    document.getElementById('timestamp-seconds').innerHTML=('Seconds '+Math.round(born/1000))
-    document.getElementById('timestamp-milliseconds').innerHTML=('Milliseconds '+(born))
-    document.getElementById('timestamp-microseconds').innerHTML=('Microseconds '+Math.round((born*1000)+Math.random()*999))
-}, 1);
-
+setInterval(()=>{
     
+    let now=new Date()
+    let dif=now-born
+    document.getElementById('timestamp-years').innerHTML=('Years '+Math.round(((((dif/1000)/60)/60)/24)/364))
+    document.getElementById('timestamp-days').innerHTML=('Days '+Math.round((((dif/1000)/60)/60)/24))
+    document.getElementById('timestamp-hours').innerHTML=('Hours '+Math.round(((dif/1000)/60)/60))
+    document.getElementById('timestamp-minutes').innerHTML=('Minutes '+Math.round((dif/1000)/60))
+    document.getElementById('timestamp-seconds').innerHTML=('Seconds '+Math.round(dif/1000))
+    document.getElementById('timestamp-milliseconds').innerHTML=('Milliseconds '+(dif))
+    document.getElementById('timestamp-microseconds').innerHTML=('Microseconds '+Math.round((dif*1000)+Math.random()*999))
+}, 1);
